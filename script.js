600dd1df03fa2b426fbfa2ce1bb519e966f7c23c
Adam Shepard <adam.w.gangl@gmail.com>
2023-02-23 11:53:44 -0600

    rendering global variables for fungusHp and attackPoints

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(onReady);
-
+let fungusHP = 100;
+let attackPoints = 100;
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
@@ -15,4 +16,9 @@ function onReady() {
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+    render()
+}
+function render(){
+    $('.ap-text').html(attackPoints + ' ' + 'Points')
+    $('.hp-text').html(fungusHP + ' ' + 'Points')
 }
